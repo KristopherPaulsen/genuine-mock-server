c4ecf466409740fcb0c1823071c00fa709541bec
Kristopher Paulsen <kpaulsen@bluehost.com>
2018-09-19 13:50:11 -0600

    added more example data

--- mockServer/Mocks/helloworld/example.js
+++ mockServer/Mocks/helloworld/example.js
@@ -1,12 +1,69 @@
 module.exports = {
-  '/api/betterbird/example': [
+  '/api/helloworld/example': [
     {
+      method: 'get',
       statusCode: 200,
       waitTime: 0,
-      method: 'get',
       response: {
-        key: "WAT",
+        key: "no params, no body, no query response",
       }
     },
   ],
-}
+  '/api/helloworld/querystring': [
+    {
+      method: 'get',
+      statusCode: 200,
+      waitTime: 0,
+      params: {
+        param: 'foo'
+      },
+      response: {
+        key: "only params response",
+      }
+    },
+  ],
+  '/api/helloworld/param/:param': [
+    {
+      method: 'delete',
+      statusCode: 200,
+      waitTime: 200,
+      query: {
+        param: 'bar',
+      },
+      response: {
+        key: "query only response",
+      }
+    },
+    {
+      method: 'get',
+      statusCode: 200,
+      waitTime: 200,
+      query: {
+        foo: 'value here, IM DIFFEENT!',
+      },
+      params: {
+        param: 'param value here',
+      },
+      response: {
+        key: "params AND query response",
+      }
+    },
+    {
+      method: 'get',
+      statusCode: 200,
+      waitTime: 200,
+      query: {
+        foo: 'value here, IM DIFFEENT!',
+      },
+      params: {
+        param: 'param value here',
+      },
+      body: {
+        baz: "still a different value",
+      },
+      response: {
+        key: "params, query, body response",
+      }
+    },
+  ]
+};
