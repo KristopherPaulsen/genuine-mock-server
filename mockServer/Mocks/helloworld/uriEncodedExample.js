afc94ddb3b61e41745c8bbb118401c6dcefaacf4
Kristopher Paulsen <kpaulsen@bluehost.com>
2018-09-20 18:38:12 -0600

    included uri encoded example

--- mockServer/Mocks/helloworld/uriEncodedExample.js
+++ mockServer/Mocks/helloworld/uriEncodedExample.js
@@ -1,12 +1,11 @@
 module.exports = {
-  '/api/betterbird/exampletwo': [
+  ['/api/helloworld/my%20test.asp%3Fname%3Dst%C3%A5le%26car%3Dsaab']: [
     {
       statusCode: 200,
       waitTime: 0,
       method: 'get',
-      query: {},
       response: {
-        key: "I am empty response!",
+        key: "Wait, a URI route actually worked?",
       }
     },
   ],
